2a0b3b3ac8392b938c1b92c08c90df8c6f456ccb
agirlprae <willaswt@gmail.com>
2018-07-21 16:26:54 +0700

    component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
@@ -7,6 +8,7 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { JumbotonComponent } from './jumboton/jumboton.component';
 import { FeatureComponent } from './feature/feature.component';
 import { NavbarComponent } from './navbar/navbar.component';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,9 +16,11 @@ import { NavbarComponent } from './navbar/navbar.component';
     JumbotonComponent,
     FeatureComponent,
     NavbarComponent
+    
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     FormsModule,
     NgbModule.forRoot()
   ],
--- src/app/feature/feature.component.ts
+++ src/app/feature/feature.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Subscriber } from 'rxjs';
 
 @Component({
   selector: 'app-feature',
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./feature.component.css']
 })
 export class FeatureComponent implements OnInit {
-
-  constructor() { }
-
+  datas
+  constructor(private http: HttpClient) { }
+  
   ngOnInit() {
+    this.http.get("http://www.mocky.io/v2/5b52f5322f0000fb0a3bb672").subscribe((data:any)=>{
+      // this.datas =  data
+      console.log(data);
+    })
   }
 
 }
