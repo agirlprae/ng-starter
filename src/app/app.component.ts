024b4100846f48c7e646da2be42d8d2ff1f33447
agirlprae <willaswt@gmail.com>
2018-07-21 14:00:59 +0700

    playing with data binding

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Ng Starter';
+
+  debugx(event){
+    let val = event,target,value
+    this.title = val
+    //console.log('presss' + event)
+  }
+    
+  }
+}
